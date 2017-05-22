b61692501f24371f453f69ebd86e0962a70ec5ab
Jack Curtis <Jack_Curtis98@hotmail.com>
2017-05-22 19:07:00 +1000

    Initial Release
    22/05/2017 - 7:06 PM
    Developer: Jack Curtis
    
    - Inital release

---
+++ AutoComment/AutoComment/frmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace AutoComment
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtComment = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDir = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fbdDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtComment
+            // 
+            this.txtComment.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtComment.Location = new System.Drawing.Point(0, 0);
+            this.txtComment.Name = "txtComment";
+            this.txtComment.Size = new System.Drawing.Size(425, 167);
+            this.txtComment.TabIndex = 0;
+            this.txtComment.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtComment);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(425, 167);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnExit);
+            this.panel2.Controls.Add(this.btnDir);
+            this.panel2.Controls.Add(this.btnWrite);
+            this.panel2.Location = new System.Drawing.Point(12, 264);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(425, 60);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(258, 17);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDir
+            // 
+            this.btnDir.Location = new System.Drawing.Point(177, 17);
+            this.btnDir.Name = "btnDir";
+            this.btnDir.Size = new System.Drawing.Size(75, 23);
+            this.btnDir.TabIndex = 2;
+            this.btnDir.Text = "&Load Dir";
+            this.btnDir.UseVisualStyleBackColor = true;
+            this.btnDir.Click += new System.EventHandler(this.btnDir_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(96, 17);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "&Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Options";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(430, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "WARNING: THIS WILL WRITE WHATEVER IS PLACE INSIDE OF THE TEXTBOX.. USE CAREFULLY";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 336);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.Text = "Auto Commenting - JCurtis";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtComment;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDir;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog fbdDir;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AutoComment/AutoComment/frmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoComment
+{
+    public partial class frmMain : Form
+    {
+
+        private string[] dirFiles; // The path and filename's
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            string initalLine = @"/*
+*
+*       Server: OurCompany
+*       AUTHOR: Jack Curtis
+*       Copyright OurCompany (C) 2017
+*
+*/"; //Inital block of text for our textbox.
+
+            txtComment.Text = initalLine; // Assigning our textbox the string.
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            if (dirFiles != null)
+            {
+                foreach (var item in dirFiles)
+                {
+                    using (StreamWriter sw = new StreamWriter(item)) // Using StreamWriter to write our text.
+                    {
+                        for (int i = 0; i < txtComment.Lines.Length; i++) // Itterating through the lines inside of our RTB.
+                        {
+                            sw.WriteLine(txtComment.Lines[i]); // Writing to File..
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a directory to get started.."); //Catching
+            }
+        }
+
+        private void btnDir_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbdDir.ShowDialog(); //Result is equal to our ShowDialog.
+            
+            if (result == DialogResult.OK)
+            {
+                dirFiles = Directory.GetFiles(fbdDir.SelectedPath); //Directory Files getting from the path of our FolderBrowserDialog.
+                frmMain.ActiveForm.Text = "Auto Commenting - JCurtis (DIR = " + fbdDir.SelectedPath + ")" //Setting title to directory to give the user some sense of security.
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
